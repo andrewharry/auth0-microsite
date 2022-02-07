28492454b3f7207d5d355ee9fda436096ce65e8e
Andrew Harry <Andrew.Harry@certegy.com.au>
2022-02-07 15:03:59 +1030

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment, Country } from './interfaces';
 export const environment: Environment = {
   production: true,
   hostDomain: '#{ idp-spa-cdn-endpoint-url-noslash }#',
-  root: '/auth0-microsite',
+  root: 'https://andrewharry.github.io/auth0-microsite',
   countryCode: '#{ idp-country-code }#',
   defaultCountry: Country.AU,
   countries: {
