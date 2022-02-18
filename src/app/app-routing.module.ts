882c0db3a4c903b394b933e93f19c5a164c9d04d
Andrew Harry <Andrew.Harry@certegy.com.au>
2022-02-18 13:39:11 +1030

    added missing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { PhoneNumberComponent } from './pages/phone-number/phone-number.component';
 import { PhoneVerifyComponent } from './pages/phone-verify/phone-verify.component';
 import { ForgotPasswordComponent } from './pages/forgot/forgot-password.component';
+import { SetPasswordComponent } from './pages/set-password/set-password.component';
 import Route from './routes';
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: Route.PhoneNumber, component: PhoneNumberComponent },
   { path: Route.PhoneVerify, component: PhoneVerifyComponent },
   { path: Route.ForgotPassword, component: ForgotPasswordComponent },
+  { path: Route.SetPassword, component: SetPasswordComponent },
   { path: '', redirectTo: `/${Route.Login}`, pathMatch: 'full' }
 ];
 
